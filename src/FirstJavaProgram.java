d7059068379fa7ca3f2386bdfe41190e9bbedc05
Joel <Joel>
2017-03-14 09:50:35 +0530

    first commit

---
+++ src/FirstJavaProgram.java
@@ -0,0 +1,20 @@
+public class FirstJavaProgram {
+
+	//main function is the first function to be executed
+	public static void main(String[] args) {
+		System.out.println("Hello World!!!");
+		System.out.println("Printing Selenium");
+		System.out.println(1234);
+		System.out.println("First Line");
+		System.out.print("Second Line");
+		System.out.println("Third Line");
+		
+		int a = 100;
+		System.out.println(a);
+		System.out.println("a");
+		a = 200;
+		System.out.println(a);
+		
+	}
+
+}
